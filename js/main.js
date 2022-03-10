3912864f19a08c664658aa33a0fb433e6b00cf33
Vitto2623 <vtr.1@icloud.com>
2022-03-10 16:52:01 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+/**
+ *
+ *
+ *
+Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
+
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
+Ricordiamo sempre l'importanza dell'integrità del dato.
+
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello?
+
+ *
+ */
+
+const gallery = [
+    {
+        item: '01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        item: '02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        item: '03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        item: '04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        item: '05.jpg',
+        title:  'Paradise',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+]
+
+let content = "";
+
+for ( let i = 0; i < gallery.length; i++){
+    content += ` 
+    <div class="my-carousel-images">
+        <img src="img/${gallery[i].item}" alt="${gallery[i].title}">
+    </div>`
+}
+
+const carouselWrapper = document.querySelector('div.my-carousel-container');
+carouselWrapper.innerHTML = content
+
+const carouselElements = document.getElementsByClassName('my-carousel-images');
+carouselElements[0].classList.add('active');
+
+// recupero bottone
+const nextButton = document.getElementsByClassName('my-next');
+console.log(nextButton)
+
+let activeElement = 0;
+
+nextButton.addEventListener('click', function(){
+    carouselElements[activeElement].classList.remove('active');
+    activeElement++;
+    carouselElements[activeElement].classList.add('active');
+})
