91045b14f8dd9d432b77c02f6855db04f9e36fca
Vitto2623 <vtr.1@icloud.com>
2022-03-11 11:02:51 +0100

    (un po') di correzione generale...

--- js/main.js
+++ js/main.js
@@ -48,17 +48,18 @@ const gallery = [
 
 
 let content = "";
+let content2 = "";
 let contatore = 0;
 
 for ( let i = 0; i < gallery.length; i++){
-    content += ` 
-    <div class="my-carousel-images">
-        <img src="img/${gallery[i].item}" alt="${gallery[i].title}">
-    </div>`
+    content2 += ` 
+    <div class="my-thumbnails position-relative">
+    <img src="img/${gallery[i].item}" class="my-border" alt="${gallery[i].title}">
+    </div>`  
 }
 
 const carouselWrapper = document.querySelector('div.my-thumbnails');
-carouselWrapper.innerHTML += content
+carouselWrapper.innerHTML += content2
 
 const carouselElements = document.querySelector('div.my-carousel-images');
 carouselElements.innerHTML = `<img src="img/${gallery[contatore].item}" alt="${gallery[contatore].title}">`
@@ -66,20 +67,28 @@ carouselElements.innerHTML = `<img src="img/${gallery[contatore].item}" alt="${g
 //dentro i pulsanti next e prev devi inserire il codice incrementando o decrementando il contatore e poi metti:
 //carouselElements.innerHTML = `<img src="img/${gallery[contatore].item}" alt="${gallery[contatore].title}">`
 
+const borderOff = document.getElementsByClassName("my-border");
+borderOff[0].classList.add('active-border');
+
+
 const nextButton = document.querySelector('.my-next');
 nextButton.addEventListener('click', function(){
+    borderOff[contatore].classList.remove('active-border');
     contatore++
     if (contatore == 5){
         contatore = 0;
     }
     carouselElements.innerHTML = `<img src="img/${gallery[contatore].item}" alt="${gallery[contatore].title}">`
+    borderOff[contatore].classList.add('active-border');
 });
 
 const previousButton = document.querySelector('.my-previous');
 previousButton.addEventListener('click', function(){
+    borderOff[contatore].classList.remove('active-border')
     contatore--
     if (contatore == -1){
         contatore = 4;
     }
     carouselElements.innerHTML = `<img src="img/${gallery[contatore].item}" alt="${gallery[contatore].title}">`
+    borderOff[contatore].classList.add('active-border');
 });
